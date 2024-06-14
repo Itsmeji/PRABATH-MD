646274e1ca57df47b0bec60118acb063af870ed6
Kurosaki H'lls <141827510+Itsmeji@users.noreply.github.com>
2024-06-14 01:51:58 +0000

    Update config-env.js

--- config-env.js
+++ config-env.js
@@ -9,8 +9,8 @@ function convertToBool(text, fault = 'true') {
 const username = fs.readFileSync('github_username.txt', 'utf8').trim()
 
 module.exports = {
-    SESSION_ID:  process.env.SESSION_ID,    
-    BOT_NUMBER:  process.env.BOT_NUMBER,
-    GITHUB_USERNAME: username,
-    GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN,
+    SESSION_ID: PRABATH-MD~ZQhz0YgJ#CNXeLpd9aoSbCBTGeJ4aPiY3P5mWjaalu6_tjlYWrIc process.env.SESSION_ID,    
+    BOT_NUMBER: 22899869601 process.env.BOT_NUMBER,
+    GITHUB_USERNAME: Itsmeji username,
+    GITHUB_AUTH_TOKEN: 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 process.env.GITHUB_AUTH_TOKEN,
 };
